4d7dac7dddbd54f3454a6450d4e22b640c54b471
Matheus Dantas <168189895+Mdantasjj@users.noreply.github.com>
2026-09-11 10:13:39 -0300

    Atualiza titulo do subcard BPTUR

--- presentation.js
+++ presentation.js
@@ -243,7 +243,7 @@ const metricDetails = {
     stats: [
       ['Déficit de efetivo do POG para as unidades especializadas', '111', 'Déficit localizado nos BPMs territoriais'],
       ['COTAM - Necessidade de efetivo pronta resposta', '110', '10 oficiais · 100 praças'],
-      ['6ª Cia/BPTUR', '50', '02 oficiais · 48 praças'],
+      ['BPTUR (Cariri e Guaramiranga)', '50', '02 oficiais · 48 praças'],
       ['Total consolidado', '271', '111 POG · 110 COTAM · 50 BPTUR']
     ],
     hideBreakdown: true,
